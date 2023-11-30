ece0cd76a6a2ca0d6ff97f44a2548866d47a07b8
Waddhanabot Yi <104647438+WBotCS@users.noreply.github.com>
2023-11-29 18:58:05 -0800

    Add files via upload

--- project.py
+++ project.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
+#Flight Analysis
 
 # Function to convert travel time to minutes
 def convert_time_to_minutes(time_str):
